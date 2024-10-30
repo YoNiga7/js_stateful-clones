533d7a1f9cf84d8bd73fa7d4785f3cf594cdeebd
Kostiantyn <kostyabondar6@gamil.com>
2024-10-30 16:56:39 +0200

    finish my task1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -8,6 +8,32 @@
  */
 function transformStateWithClones(state, actions) {
   // write code here
+  let currentState = { ...state };
+  const stateHistory = [];
+
+  actions.forEach((action) => {
+    switch (action.type) {
+      case 'addProperties':
+        currentState = { ...currentState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        currentState = { ...currentState };
+        action.keysToRemove.forEach((key) => delete currentState[key]);
+        break;
+
+      case 'clear':
+        currentState = {};
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
+    }
+
+    stateHistory.push(currentState);
+  });
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
